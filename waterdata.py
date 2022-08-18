3efe3cd89de427f3fd73423dcf3a9155a8c38494
jtdub <amascuba@gmail.com>
2022-08-17 23:23:01 -0500

    refactor script to make it easier to add sites

--- waterdata.py
+++ waterdata.py
@@ -1,12 +1,58 @@
 #!/usr/bin/env python3
 
 import csv
-from pprint import pprint
 from typing import List
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import requests
+
+sites = {
+    "amistad": {
+        "url": "https://www.ibwc.gov/wad/450800_a.txt",
+        "fields": [
+            "date",
+            "time|cst",
+            "elevation|meters",
+            "storage|tcm",
+            "surface area|ha",
+        ],
+        "models": [
+            {"x": "date", "y": "storage|tcm", "pos": [0, 0]},
+            {"x": "date", "y": "surface area|ha", "pos": [0, 1]},
+        ],
+    },
+    "sanfelipe": {
+        "url": "https://www.ibwc.gov/wad/453000_a.txt",
+        "fields": [
+            "date",
+            "time|cst",
+            "stage|meters",
+            "discharge|cms",
+            "preciptation|amm",
+        ],
+        "models": [
+            {"x": "date", "y": "discharge|cms", "pos": [1, 0]},
+            {"x": "date", "y": "preciptation|amm", "pos": [1, 1]},
+        ],
+    },
+    "pecos": {
+        "url": "https://www.ibwc.gov/wad/447410_a.txt",
+        "fields": [
+            "date",
+            "time|cst",
+            "stage|meters",
+            "discharge|cms",
+            "preciptation|amm",
+        ],
+        "models": [
+            {"x": "date", "y": "discharge|cms", "pos": [2, 0]},
+            {"x": "date", "y": "preciptation|amm", "pos": [2, 1]},
+        ],
+    },
+}
+cols = 2
+rows = len(sites.keys())
 
 
 def get_data(url: str) -> str:
@@ -31,57 +77,21 @@ def create_csv(data: List[list], filename: str, fields: list) -> None:
 
 
 if __name__ == "__main__":
-    amistad = get_data("https://www.ibwc.gov/wad/450800_a.txt")
-    sanfelipe = get_data("https://www.ibwc.gov/wad/453000_a.txt")
-    amistad_data = structured_text_to_csv(amistad)
-    sanfelipe_data = structured_text_to_csv(sanfelipe)
-    create_csv(
-        amistad_data,
-        "amistad.csv",
-        [
-            "Date",
-            "Time|CST",
-            "Elevation|Meters",
-            "Storage|Thousand Cubic Meters",
-            "Surface Area|Hundred Acres",
-        ],
-    )
-    create_csv(
-        sanfelipe_data,
-        "sanfelipe.csv",
-        [
-            "Date",
-            "Time|CST",
-            "Stage|Meters",
-            "Discharge|Cubic Meters per Second",
-            "Preciptation|Accumulation Milimeters",
-        ],
-    )
-    amistad_pd = pd.read_csv("amistad.csv")
-    sanfelipe_pd = pd.read_csv("sanfelipe.csv")
-    fig, axes = plt.subplots(nrows=2, ncols=2)
-    amistad_pd.plot(
-        x="Date",
-        y="Surface Area|Hundred Acres",
-        ax=axes[0, 0],
-        title="Lake Amistad | Surface Area",
-    )
-    amistad_pd.plot(
-        x="Date",
-        y="Storage|Thousand Cubic Meters",
-        ax=axes[0, 1],
-        title="Lake Amistad | Storage",
-    )
-    sanfelipe_pd.plot(
-        x="Date",
-        y="Discharge|Cubic Meters per Second",
-        ax=axes[1, 0],
-        title="San Felipe | Discharge",
-    )
-    sanfelipe_pd.plot(
-        x="Date",
-        y="Preciptation|Accumulation Milimeters",
-        ax=axes[1, 1],
-        title="San Felipe | Precipitation",
-    )
+    fig, axes = plt.subplots(nrows=rows, ncols=cols)
+
+    for site, data in sites.items():
+        url_data = get_data(data.get("url"))
+        csv_data = structured_text_to_csv(url_data)
+        create_csv(csv_data, f"{site}.csv", data.get("fields"))
+        site_pd = pd.read_csv(f"{site}.csv")
+
+        for model in data.get("models"):
+            pos = model.get("pos")
+            site_pd.plot(
+                x=model.get("x"),
+                y=model.get("y"),
+                ax=axes[pos[0], pos[1]],
+                title=f"{site.lower()} - {model.get('y').lower()}",
+            )
+
     plt.show()
